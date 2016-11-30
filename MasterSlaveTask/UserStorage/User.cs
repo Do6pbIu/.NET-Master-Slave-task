b5f4a73653a79f688c9ac2d8ae85490dae7c7ae5
PavelKolesnikov <kolesnikovpaveled@gmail.com>
2016-11-30 16:38:37 +0300

    Add files via upload

---
+++ MasterSlaveTask/UserStorage/User.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStorage
+{
+    [Serializable]
+    public class User : IEquatable<User>
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Id { get;}
+        public Gender Sex { get; private set; }
+        public List<VisaRecord> visaCards { get; private set; }
+
+        #region Equals
+        public bool Equals(User other)
+        {
+            if (other == null)
+                return false;
+            if (this.Id == other.Id) return true;
+            else
+                return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            User userObj = obj as User;
+            if (userObj == null)
+                return false;
+            else return Equals(userObj);
+        }
+
+        public override int GetHashCode()
+        {
+            if (Id != null) return Id.GetHashCode();
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString()+" "
+                                    + FirstName.ToString() + " " 
+                                    +LastName.ToString()+" "
+                                    +Sex.ToString()+" "
+                                    +visaCards.Count.ToString();            
+        }
+        #endregion
+
+        #region Constructors
+        private User() { }
+
+        public User(string fName, string lName, string Id, Gender sex)
+        {
+            if (fName == null || lName == null || Id == null) throw new ArgumentException();
+            visaCards = new List<VisaRecord>();
+        }
+        #endregion
+
+        public void ChangeGender(Gender newSex)
+        {
+            this.Sex = newSex;
+        }
+
+        public void AddVisaCard(VisaRecord newVisa)
+        {
+            if (visaCards.IndexOf(newVisa) == -1) visaCards.Add(newVisa);
+        }
+    }
+
+    public struct VisaRecord
+    {
+        private string country;
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public VisaRecord (string ctr, DateTime sDate, DateTime eDate)
+        {
+            if (ctr == null || eDate < DateTime.Now) throw new ArgumentException();            
+            country = ctr;
+            startDate = sDate;
+            endDate = eDate;
+        }
+
+        public string Country => country;
+        public DateTime StartDate => startDate;
+        public DateTime EndDate => endDate;
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
---
+++ MasterSlaveTask/UserStorage/UserService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStorage
+{
+    [Serializable]
+    public class UserService
+    {
+        private IList<User> users;
+
+        [NonSerialized]
+        private IIdGenerator idGenerator;
+
+        [NonSerialized]
+        private IUserValidator userValidator;
+
+        private string lastId;
+        #region Constructors
+        public UserService(IIdGenerator generator, IUserValidator validator, IList<User> existingUsers)
+        {
+            if (existingUsers == null || generator == null || validator == null)
+                throw new ArgumentException();
+            users = existingUsers;
+            idGenerator = generator;
+            userValidator = validator;
+        }
+
+        public UserService(IIdGenerator generator, IUserValidator validator)
+        {
+            if (generator == null || validator == null)
+                throw new ArgumentException();
+            users = new List<User>();
+            idGenerator = generator;
+            userValidator = validator;
+        }
+
+        private UserService() { }
+        #endregion
+
+        #region CRUDuser
+        /// <summary>
+        /// Adds created user to storage
+        /// </summary>
+        /// <param name="newUser"></param>
+        /// <returns>User's id if he was valid and added</returns>
+        public string AddUser(User newUser)
+        {
+            if (userValidator.UserIsValid(newUser))
+            {
+                users.Add(newUser);
+                return newUser.Id;
+            }
+            else
+                throw new ArgumentException();
+        }
+
+        /// <summary>
+        /// Creates new user and adds him to storage
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="sex"></param>
+        /// <returns>User's id if he was added to storage</returns>
+        public string CreateUser(string firstName, string lastName, Gender sex)
+        {
+            User newUser = new User(firstName, lastName, idGenerator.NextId(), sex);
+            lastId = newUser.Id;
+            return (AddUser(newUser));
+        }
+
+        /// <summary>
+        /// Removes specified user from storage
+        /// </summary>
+        /// <param name="dUser"></param>
+        public void DeleteUser(User dUser)
+        {
+            users.Remove(dUser);
+        }
+
+        /// <summary>
+        /// Searches users with specified cinditions
+        /// </summary>
+        /// <param name="match"></param>
+        /// <returns></returns>
+        public IEnumerable<User> SearchForUser(Func<User, bool> match) => users.Where(match);
+        #endregion
+
+        /// <summary>
+        /// Changes the strategy to generate and Id
+        /// </summary>
+        /// <param name="newGenerator"></param>
+        public void ChangeIdGenerator(IIdGenerator newGenerator)
+        {
+            if (newGenerator == null)
+                throw new ArgumentException();
+            else
+                idGenerator = newGenerator;
+        }
+
+        /// <summary>
+        /// Chenges the rules of validation
+        /// </summary>
+        /// <param name="newValidator"></param>
+        public void ChangeUserValidator(IUserValidator newValidator)
+        {
+            if (newValidator == null) throw new ArgumentException();
+            else
+                userValidator = newValidator;
+        }        
+    }
+}
